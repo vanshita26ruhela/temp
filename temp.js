1f300fd2bd786081ceb13ae5e6b709e955077077
Vanshita ruhela <121929927+vanshita26ruhela@users.noreply.github.com>
2023-10-27 19:56:13 +0530

    first commit

---
+++ temp.js
@@ -0,0 +1,27 @@
+const convertfahrenheit = () => {
+    var celsius = parseFloat(document.querySelector("#celsius").value);
+
+
+    var fahrenheitResult = (1.8 * celsius) + 32;
+    var a = fahrenheitResult.toFixed(2);
+
+    document.querySelector("#fahrenheit").value = a;
+    document.querySelector("#celsius").value = celsius;
+}
+
+
+const convertcelsius = () => {
+    var fahrenheit = parseFloat(document.querySelector("#fahrenheit").value);
+
+
+    var celsiusResult = (fahrenheit - 32) / 1.8;
+    var b = celsiusResult.toFixed(2);
+
+    document.querySelector("#celsius").value = b;
+    document.querySelector("#fahrenheit").value = fahrenheit;
+
+}
+const reset = () => {
+    document.querySelector("#celsius").value = " ";
+    document.querySelector("#fahrenheit").value = " ";
+}
